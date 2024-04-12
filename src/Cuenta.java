eb7ebf7e4d6127151fd1f78ecc67c197dfa8f2e6
Enfermatik <anniemarie27@gmail.com>
2024-04-12 11:24:54 -0400

    subida de archivos

---
+++ src/Cuenta.java
@@ -0,0 +1,11 @@
+public class Cuenta {
+    private double saldo;
+
+   private void retira(double valor){
+//        logica de retiro
+
+        int getsaldo(){
+        return saldo;
+
+    }
+}
---
+++ src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    public static void main(String[] args) {
+        Cuenta c1= new Cuenta();
+        c1.getsaldo= 300;
+        c1.retirar(100);
+    }
+}
